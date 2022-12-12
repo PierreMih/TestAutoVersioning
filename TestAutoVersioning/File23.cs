1fdfb4eda35509d0223339cdf0f64ca32d77e33f
p-mih <p-mih@LPC01170>
2022-12-12 16:48:03 +0100

    fix: Hello world

---
+++ TestAutoVersioning/File23.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestAutoVersioning
+{
+    internal class File23
+    {
+    }
+}
